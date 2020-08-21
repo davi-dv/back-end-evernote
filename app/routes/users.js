0851793ad261c8005aa9764a9485bfeb80a4d30c
Davi silva <davisilva@unochapeco.edu.br>
2020-08-21 16:24:15 -0300

    criado o endpoint do login para o usuario junto com o jwt para retornar um token ao realizar o login,instalado lib dotenv,para criar uma variavel ambiente para gerar o token ao efetuar login,usando metodo do model user,para verificar se o password passado e igual ao que esta no banco de dados

--- app/routes/users.js
+++ app/routes/users.js
@@ -1,6 +1,11 @@
 var express = require('express');
 var router = express.Router();
 const User = require('../../models/user')
+const jwt = require('jsonwebtoken')
+require ('dotenv').config();
+
+const secret = process.env.JWT_TOKEN
+
 
 //esse endpoint registra o usuário
 router.post('/register',async(req,res)=>{
@@ -12,6 +17,34 @@ router.post('/register',async(req,res)=>{
     res.status(200).json(user);
   } catch (error) {
     res.status(500).json({error:'Error ao registrar novo usuário'})
+  }
+})
+
+//esse endpoint faz o login do usuario e cria um token para o mesmo com jwt
+router.post('/login',async(req,res)=>{
+  const {email,password} = req.body
+ 
+
+  try {
+    const  user = await User.findOne({email})
+
+    if(!user)
+        res.status(401).json({error:'Email ou senha incorretos!'})
+    else{
+      //acessa funcão criada no model user.js e compara
+      user.isCorrectPassword(password,function(err,same){
+        if (!same)
+        res.status(401).json({error:'Email ou senha incorretos!'})
+        //se der certo gera um token para o usuário
+        else{
+          const token = jwt.sign({email},secret,{expiresIn:'30d'});
+          res.json({user:user,token:token})
+        }
+      })
+    }    
+  } catch (error) {
+    res.status(500).json({error:'Erro interno,tente mais tarde'})
+    
   }
 })
 
--- models/user.js
+++ models/user.js
@@ -26,4 +26,15 @@ userSchema.pre('save', function (next) {
         )
     }
 })
+
+//compara senha com o hash no banco
+userSchema.methods.isCorrectPassword = function(password,callback){
+    bcrypt.compare(password,this.password,function(err,same){
+        if(err)
+        callback(err)
+        else
+            callback(err,same)
+        
+    })
+}
 module.exports = mongoose.model('User', userSchema)
