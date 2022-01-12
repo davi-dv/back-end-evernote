80b5f125430c7901271df62d9bc23571572b6ad9
davi <davisilva@amo.delivery>
2022-01-12 20:09:33 -0300

    adicionado variavel de ambiente para fazer deploy no heroku

--- config/database.js
+++ config/database.js
@@ -1,13 +1,14 @@
-const mongoose = require('mongoose');
-mongoose.Promise=global.Promise
+const mongoose = require("mongoose");
+mongoose.Promise = global.Promise;
+require("dotenv").config();
 
 //mongoose faz conexão com banco mongodb
-mongoose.connect('mongodb://localhost/note-back-end',{
-  useNewUrlParser:true,
-  useUnifiedTopology:true,
-  useCreateIndex:true
-}).then(()=>{
-    console.log('Conectado com sucesso')
-}).catch((err)=>{
-    console.log(err)
-})
+const MONGO_URL = process.env.MONGO_URL;
+mongoose
+  .connect(MONGO_URL, {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+    useCreateIndex: true,
+  })
+  .then(() => console.log("connection succesful"))
+  .catch((err) => console.error(err));
