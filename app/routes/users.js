5eb0e89710e9ce7c382660fe90b13b988e5306ce
davi <davisilva@amo.delivery>
2022-01-12 14:07:52 -0300

    criado novo endpoint para atualizar email do usuario, outro para atualizar a senha, e outro para deletar um usuario

--- app/routes/users.js
+++ app/routes/users.js
@@ -47,5 +47,43 @@ router.post('/login',async(req,res)=>{
     
   }
 })
+// esse endpoint recebe o email de um usuario e atualiza 
+router.put('/', withAuth, async function(req, res) {
+  const { name, email } = req.body;
 
+  try {
+    var user = await User.findOneAndUpdate(
+      {_id: req.user._id}, 
+      { $set: { name: name, email: email}}, 
+      { upsert: true, 'new': true }
+    )
+    res.json(user);
+  } catch (error) {
+    res.status(401).json({error: error});
+  }
+});
+// esse endpoint recebe a senha do usuario e atualiza ela
+router.put('/password', withAuth, async function(req, res) {
+  const { password } = req.body;
+
+  try {
+    var user = await User.findOne({_id: req.user._id})
+    user.password = password
+    user.save()
+    res.json(user);
+  } catch (error) {
+    res.status(401).json({error: error});
+  }
+});
+// esse endpoint deleta um usuario pelo id
+router.delete('/', withAuth, async function(req, res) {
+  try {
+    let user = await User.findOne({_id: req.user._id });
+    await user.delete();
+    res.json({message: 'OK'}).status(201);
+  } catch (error) {
+    res.status(500).json({error: error});
+  }
+});
+	
 module.exports = router;
